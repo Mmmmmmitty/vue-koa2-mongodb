a8754b15bf3a75a7c000c422e1d4d5ebbd9fe387
liuwei <liuwei@cimgroup.cn>
2019-05-08 18:31:23 +0800

    11

--- SmileVue/src/components/pages/ShoppingMall.vue
+++ SmileVue/src/components/pages/ShoppingMall.vue
@@ -14,48 +14,78 @@
         </van-col>
       </van-row>
     </div>
+    <!--AD banner area-->
+    <div class="ad-banner">
+      <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
+    </div>
     <!--swipwer area-->
     <div class="swiper-area">
       <van-swipe :autoplay="3000">
         <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
-          <img class="swipe-item-img" v-lazy="banner.imageUrl" width="100%">
+          <img class="swipe-item-img" v-lazy="banner.image" width="100%">
         </van-swipe-item>
       </van-swipe>
     </div>
+    <div class="type-bar">
+      <div v-for="(cate,index) in category" :key="index">
+        <img v-lazy="cate.image" width="90%">
+        <span>{{cate.mallCategoryName}}</span>
+      </div>
+    </div>
+    <!--Recommend goods area-->
+    <div class="recommend-area">
+      <div class="recommend-title">商品推荐</div>
+      <div class="recommend-body"></div>
+    </div>
+    <!--swiper-->
+    <swiper :options="swiperOption">
+      <swiper-slide v-for=" (item ,index) in recommendGoods" :key="index">
+        <div class="recommend-item">
+          <img :src="item.image" width="80%">
+          <div>{{item.goodsName}}</div>
+          <div>￥{{item.price}} (￥{{item.mallPrice}})</div>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import swipe1 from "../../assets/images/swipe/swipe1.jpg";
-import swipe2 from "../../assets/images/swipe/swipe2.jpg";
-import swipe3 from "../../assets/images/swipe/swipe3.jpg";
+import url from "@/serviceAPI.config.js"
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
   data() {
     return {
-      bannerPicArray: [
-        {
-          imageUrl: swipe1
-        },
-        {
-          imageUrl: swipe2
-        },
-        {
-          imageUrl: swipe3
-        }
-      ]
+      swiperOption: {
+        autoplay: false
+      },
+      bannerPicArray: [],
+      adBanner: "",
+      recommendGoods: [],
+      category: []
     };
   },
-  created(){
+  components: {
+    swiperSlide,
+    swiper
+  },
+  created() {
     axios({
-        url: 'https://www.easy-mock.com/mock/5cd1577f6011ab3ddac90971/SmileVue/index',
-        method: 'get',
+      url: url.getShoppingMallInfo,
+      method: "get"
     })
-    .then(response => {
-        console.log(response)
-    })
-    .catch((error) => {     
-    })
+      .then(response => {
+        if (response.status == 200) {
+          console.log(response.data.data);
+          this.adBanner = response.data.data.advertesPicture; //获得广告图片
+          this.bannerPicArray = response.data.data.slides; //轮播图片
+          this.category = response.data.data.category;
+          this.recommendGoods = response.data.data.recommend; //推荐商品
+        }
+      })
+      .catch(error => {});
   }
 };
 </script>
@@ -87,9 +117,43 @@ export default {
   width: 100%;
   clear: both;
 }
-.swipe-item-img{
+.swipe-item-img {
   display: inline-block;
   width: 7.5rem;
   height: 4.2rem;
+}
+.type-bar {
+  background-color: #fff;
+  margin: 0 0.2rem 0.2rem 0.2rem;
+  border-radius: 0.3rem;
+  font-size: 0.28rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+.type-bar div {
+  padding: 0.3rem;
+  font-size: 0.24rem;
+  text-align: center;
+}
+.recommend-area {
+  background-color: #fff;
+  margin-top: 0.1rem;
+}
+.recommend-title {
+  border-bottom: 1px solid #eee;
+  font-size: 0.28rem;
+  padding: 0.1rem;
+  color: #e5017d;
+}
+.recommend-body {
+  border-bottom: 1px solid #eee;
+}
+
+.recommend-item {
+  width: 99%;
+  border-right: 1px solid #eee;
+  font-size: 12px;
+  text-align: center;
 }
 </style>
---
+++ SmileVue/src/serviceAPI.config.js
@@ -0,0 +1,7 @@
+const BASEURL = "https://www.easy-mock.com/mock/5cd1577f6011ab3ddac90971/SmileVue/"
+const URL = {
+    getShoppingMallInfo:BASEURL+'index',
+    getGoodsInfo:BASEURL+'getGoodsInfo'
+}
+
+module.exports = URL
