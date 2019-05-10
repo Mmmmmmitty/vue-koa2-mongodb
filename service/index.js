a7c2ce03e5b48c647bd50d4200422b120cc886d4
liuwei <liuwei@cimgroup.cn>
2019-05-10 18:29:33 +0800

    11

--- service/index.js
+++ service/index.js
@@ -1,21 +1,29 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
 const mongoose = require('mongoose')
 const { connect, initSchemas } = require('./database/init.js')
+let user = require('./appApi/user.js')
+let router = new Router();
+router.use('/user',user.routes())
+app.use(router.routes())
+app.use(router.allowedMethods())
 
   //立即执行函数
   ; (async () => {
     await connect()
     initSchemas()
     const User = mongoose.model('User')
-    let oneUser = new User({ name: 'liuwei11', password: '123456' })
+    let oneUser = new User({ "userName": 'liuwei11', "password": '123456' })
 
-    // oneUser.save().then(() => {
-    //   console.log('插入成功')
-    // })
-    oneUser.save(function (err) {
-      if(err) console.log(err)
-    });
+    await oneUser.save().then(() => {
+      console.log('插入成功')
+    }).catch((error)=>{
+      console.log(error)
+    })
+    // oneUser.save(function (err) {
+    //   if(err) console.log(err)
+    // });
     let users = await User.findOne({}).exec()
 
     console.log('------------------')
